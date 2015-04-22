44525904bda9b90f9bcbb89f6bf535392935cd4e
Collin Fair <cpf@cpfx.ca>
2015-04-22 16:52:51 -0400

    Reduce global shoddiness quotient

--- faux_pas.js
+++ faux_pas.js
@@ -1,34 +1,35 @@
 $(function(){
-	var frame = $("iframe").get(0);
-	if (!frame) {
-		frame = $("frame", parent.document).get(1);
-	}
-	frame.contentWindow.addEventListener("hashchange", function(){
-		var in_watchfaces = frame.contentWindow.location.hash.indexOf("watchfaces") >= 0;
-		$(".header-area .apps").toggleClass("active", !in_watchfaces);
-		$(".header-area .faces").toggleClass("active", in_watchfaces);
+    var frame = $("iframe").get(0);
+    if (!frame) {
+        frame = $("frame", parent.document).get(1);
+    }
 
-		var app_id = frame.contentWindow.location.hash.match(/[a-f0-9]{24}/);
-		$(".header-link").toggle(!!app_id);
-		if (app_id) {
-			$(".header-link a").attr("href", "https://apps.getpebble.com/applications/" + app_id)
-		}
-	}, false);
+    frame.contentWindow.addEventListener("hashchange", function(){
+        var location = frame.contentWindow.location
+        var in_watchfaces = location.hash.indexOf("watchfaces") >= 0;
+        $(".header-area .apps").toggleClass("active", !in_watchfaces);
+        $(".header-area .faces").toggleClass("active", in_watchfaces);
 
-	$(".header-back").click(function(){
-		frame.contentWindow.history.go(-1);
-	});
-	$(".header-search").click(function(){
-		frame.contentWindow.location.hash = "/search/watchapps";
-	});
-	$(".header-area .apps").click(function(){
-		frame.contentWindow.location.hash = "/";
-	});
-	$(".header-area .faces").click(function(){
-		frame.contentWindow.location.hash = "/watchfaces";
-	});
-	$(".fullscreen-button").click(function(){
-		$("body").addClass("fullscreen");
-	});
+        var app_id = location.hash.match(/[a-f0-9]{24}/);
+        $(".header-link").toggle(!!app_id);
+        if (app_id) {
+            $(".header-link a").attr("href", "https://apps.getpebble.com/applications/" + app_id)
+        }
+    }, false);
 
+    $(".header-back").click(function(){
+        frame.contentWindow.history.go(-1);
+    });
+
+    $(".header-search").click(function(){
+        frame.contentWindow.location.hash = "/search/watchapps";
+    });
+
+    $(".header-area .apps").click(function(){
+        frame.contentWindow.location.hash = "/";
+    });
+
+    $(".header-area .faces").click(function(){
+        frame.contentWindow.location.hash = "/watchfaces";
+    });
 });
