f03560c61bd4b09103f3da33cbf69a37b48d5c42
Collin Fair <cpf@cpfx.ca>
2015-05-22 22:03:53 -0400

    Script to generate store_boot.html

---
+++ generate_store_boot.py
@@ -0,0 +1,13 @@
+import requests
+
+# Get the stock store boot page
+boot_req = requests.get("https://apps-prod.getpebble.com/en_US/")
+
+boot_f = open("store_boot.html", "w")
+
+# Bake in our cookies
+cookie_set = """<script type="text/javascript">""" + ";".join(["document.cookie='%s=%s'"  % pair for pair in boot_req.cookies.items()]) + """</script>"""
+boot_f.write(boot_req.text.replace("<script", cookie_set + "<script", 1))
+
+# Append our patches
+boot_f.write("""<script type="text/javascript" src="/store_patch.js"></script><link rel="stylesheet" type="text/css" href="/store_patch.css">""")
