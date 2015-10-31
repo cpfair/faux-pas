fa2cf8e047b161ff97963b4874b6c805749cb678
Collin <cpf@cpfx.ca>
2015-10-31 12:47:23 -0400

    Py2k fix for store_boot generation

--- generate_store_boot.py
+++ generate_store_boot.py
@@ -7,7 +7,7 @@ boot_f = open("store_boot.html", "w")
 
 # Bake in our cookies
 cookie_set = """<script type="text/javascript">""" + ";".join(["document.cookie='%s=%s'"  % pair for pair in boot_req.cookies.items()]) + """</script>"""
-boot_f.write(boot_req.text.replace("<script", cookie_set + "<script", 1))
+boot_f.write(boot_req.text.replace("<script", cookie_set + "<script", 1).encode("utf-8"))
 
 # Append our patches
 boot_f.write("""<script type="text/javascript" src="/store_patch.js"></script><link rel="stylesheet" type="text/css" href="/store_patch.css">""")
